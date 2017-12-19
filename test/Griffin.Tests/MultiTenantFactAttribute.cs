5bd99f88c0e28bd1e8c8261d5bf2c25e2bcc96ec
Hui-Jie Gao <gaohuijie@hotmail.com>
2017-12-19 14:29:32 +0800

    Initial Check In #5

---
+++ test/Griffin.Tests/MultiTenantFactAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Griffin.Tests
+{
+    public sealed class MultiTenantFactAttribute : FactAttribute
+    {
+        public MultiTenantFactAttribute()
+        {
+            if (!GriffinConsts.MultiTenancyEnabled)
+            {
+                Skip = "MultiTenancy is disabled.";
+            }
+        }
+    }
+}
